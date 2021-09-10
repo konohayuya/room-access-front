3540f1f91ffac464dd29c41b23b9856494328713
konoha <konohayuya1222@gmail.com>
2021-09-10 15:03:34 +0900

    component fontawesome

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+/* FontAwesome */
+library.add(faCoffee);
+app.component('font-awesome-icon', FontAwesomeIcon);
+/* FontAwesome */
+
+app.mount('#app');
